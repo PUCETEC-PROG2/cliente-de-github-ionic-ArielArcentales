a6082d8251a824f0e2b2c7d05197cd369594cc07
Ariel Arcentales <amarcentales@puce.edu.ec>
2025-12-18 23:38:05 -0500

    Implementación del método GET para obtener la informacion de usuario

--- src/services/GithubService.ts
+++ src/services/GithubService.ts
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { RepositoryItem } from "../interfaces/RepositoryItem";
+import { UserInfo } from "../interfaces/UserInfo";
 
-const GITHUB_API_URL = "https://api.github.com";
-const GITHUB_API_TOKEN = "xxxxxxxxxxxxxx"; 
+const GITHUB_API_URL = import.meta.env.VITE_GITHUB_API_URL || "https://api.github.com";
+const GITHUB_API_TOKEN = import.meta.env.VITE_GITHUB_API_TOKEN || "";
 
-
+/**
+ * Obtener repositorios del usuario autenticado
+ * @returns Repositorios del usuario
+ */
 export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
   try {
     const response = await axios.get(`${GITHUB_API_URL}/user/repos`, {
@@ -34,7 +38,10 @@ export const fetchRepositories = async (): Promise<RepositoryItem[]> => {
   }
 };
 
-
+/**
+ * Crear repositorios
+ * @param repo 
+ */
 export const createRepository = async (repo : RepositoryItem): Promise<void> => {
   try {
     const response = await axios.post(`${GITHUB_API_URL}/user/repos`, repo, {
@@ -45,5 +52,19 @@ export const createRepository = async (repo : RepositoryItem): Promise<void> => 
     console.log("Repositorio ingresado ", response.data);
   } catch (error) {
     console.error("Error creating repository:", error);
+  }
+};
+
+export const getUserInfo = async (): Promise<UserInfo | null> => {
+  try {
+    const response = await axios.get(`${GITHUB_API_URL}/user`, {
+      headers: {
+        Authorization: `Bearer ${GITHUB_API_TOKEN}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching user info:", error);
+    return null;
   }
 };
